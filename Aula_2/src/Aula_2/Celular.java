062dd2540baadfe559c6c7b79188f1c6e034ba61
LAB2-PC12 <LAB2-PC12@DESKTOP-OLCHRMS>
2024-02-05 20:49:43 -0300

    primeiro commit

---
+++ Aula_2/src/Aula_2/Celular.java
@@ -0,0 +1,32 @@
+package Aula_2;
+
+public class Celular {
+	public String modelo;
+	public int Ano;
+	public String Cor;
+	public String Cameras;
+	public String Marca;
+	public String Linha;
+	
+	public void Rapido () {
+		System.out.println("O Celular: " + modelo + "Rapido");
+	}
+	
+	public void GetCelular() {
+		System.out.println(
+				"Modelo: " + modelo + 
+				"\n Ano:" + Ano +
+				"\n Marca: " + Marca + 
+				"\n Cor: " + Cor +
+				"\n linha:" + Linha +
+				"\n TipoCamera:" + Cameras
+				
+				);
+		
+	}
+
+}
+	
+	
+
+}
---
+++ Aula_2/src/Aula_2/Main.java
@@ -0,0 +1,18 @@
+package Aula_2;  
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Celular Celular_1 = new Celular();
+				Celular_1.Cameras  =" Camera tripla";
+				Celular_1.Ano = 2021;
+				Celular_1.modelo = "A14";
+				Celular_1.Cor = "branco";
+				Celular_1.Marca = "Samsumg";
+				Celular_1.Linha = "Galaxy";
+				Celular_1.GetCelular();
+				
+	}
+	
+}

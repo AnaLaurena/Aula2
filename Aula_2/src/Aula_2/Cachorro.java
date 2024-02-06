fc5454f85e6a0f70e96e0be2a899d0230b0c9e57
LAB2-PC12 <LAB2-PC12@DESKTOP-OLCHRMS>
2024-02-05 21:53:01 -0300

    implantação de encapsulamento

--- Aula_2/src/Aula_2/Cachorro.java
+++ Aula_2/src/Aula_2/Cachorro.java
@@ -1,10 +1,44 @@
 package Aula_2;
 
 public class Cachorro {
-	public String nome;
-	public String raca;
-	public int idade;
-	public String cor;
+	private String nome;
+	private String raca;
+	private int idade;
+	private String cor;
+	
+	public String getnome() {
+		return nome;
+	}
+	
+	public String getRaca() {
+		return raca;
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+	
+	public int getidade() {
+		return idade;
+	}
+	
+	public void setnome (String nome) {
+		this.nome =  nome;
+	}
+	
+	public void setraca (String  raca) {
+		this.raca =  raca;
+	}
+	
+	public void setidade (int idade) {
+		this.idade =  idade;
+	}
+	
+	public void setcor (String cor) {
+		this.cor =  cor;
+	}
+	
+	
 	
 	public void latir () {
 		System.out.println("O cachorro: " + nome + "latiu");
